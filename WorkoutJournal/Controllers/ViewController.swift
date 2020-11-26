b1d0be7ee13f5b85bf82502ca7173233cba60be3
vlad-bilyk <bilyk.v@ucu.edu.ua>
2020-11-26 14:10:10 +0200

    testing

--- WorkoutJournal/Controllers/ViewController.swift
+++ WorkoutJournal/Controllers/ViewController.swift
@@ -12,7 +12,26 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        sleep(3)
+        print("HELLo")
+        
+        let jm = JournalManager.shared
+        
+        let r: Activity = Running(distance: 1000, duration: 60)
+        print(r.name)
+        
+        let rId = jm.createEntry(activity: r, date: Date())
+        
+        jm.printEntryInfo(entryId: rId)
+        jm.setEntryDate(entryId: rId, newDate: Date(timeIntervalSince1970: 150))
+        jm.printEntryInfo(entryId: rId)
+        jm.setEntryActivityDistance(entryId: rId, newDistance: 20000)
+        jm.setEntryActivityDuration(entryId: rId, newDuration: 6000)
+        jm.printEntryInfo(entryId: rId)
+        jm.setEntryActivityRepetitions(entryId: rId, newRepetitions: 20)
+        jm.printEntryInfo(entryId: rId)
+        
     }
 
 
