bd2fa1e4de2b9fe8cb3e54294c3dfa4af5f78b11
vlad-bilyk <bilyk.v@ucu.edu.ua>
2020-12-08 12:55:58 +0200

    added Storyboarded protocol and Coordinator class

---
+++ WorkoutJournal/Coordinators/Coordinator.swift
@@ -0,0 +1,14 @@
+//
+//  Coordinator.swift
+//  WorkoutJournal
+//
+//  Created by Vlad Bilyk on 07.12.2020.
+//  Copyright © 2020 Vlad Bilyk. All rights reserved.
+//
+
+import Foundation
+
+
+class Coordinator {
+    
+}
---
+++ WorkoutJournal/Protocols/Storyboarded.swift
@@ -0,0 +1,25 @@
+//
+//  Storyboarded.swift
+//  WorkoutJournal
+//
+//  Created by Vlad Bilyk on 07.12.2020.
+//  Copyright © 2020 Vlad Bilyk. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol Storyboarded {
+    
+    static func instantiate(storyboardName: String) -> Self
+    
+}
+
+extension Storyboarded where Self: UIViewController {
+    
+    static func instantiate(storyboardName: String) -> Self {
+        let id = String(describing: self)
+        return UIStoryboard(name: storyboardName, bundle: nil).instantiateViewController(withIdentifier: id) as! Self
+    }
+    
+}
