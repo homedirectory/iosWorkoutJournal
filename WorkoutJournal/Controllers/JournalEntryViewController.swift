b70c7834046c61dfe80701b1716e71ad7418dfc2
vlad-bilyk <bilyk.v@ucu.edu.ua>
2020-12-10 18:19:18 +0200

    added gestureRecognizer and selectedActivityInstance property

--- WorkoutJournal/Controllers/JournalEntryViewController.swift
+++ WorkoutJournal/Controllers/JournalEntryViewController.swift
@@ -9,26 +9,24 @@
 import Foundation
 import UIKit
 
-#warning("TODO: duration, distance and repetitions cells need to be fixed")
-// TODO: TODO
-//      1) duration, distance and repetitions cells need to be fixed
-//          b) you cant edit inapropriate details for a particular activity type
-
 
 class JournalEntryViewController: UIViewController, Storyboarded {
     
     weak var coordinator: Coordinator?
     var journalManager: JournalManager?
     private var detailsCellLabels = ["", "Duration", "Distance", "Repetititons"]
+    private var selectedActivityInstance: Activity? = nil
     
     lazy var activityCell = self.tableView.cellForRow(at: [0, 0]) as! JournalEntryTableViewActivityCell
     lazy var durationCell = self.tableView.cellForRow(at: [0, 1]) as! JournalEntryTableViewDetailsCell
     lazy var distanceCell = self.tableView.cellForRow(at: [0, 2]) as! JournalEntryTableViewDetailsCell
     lazy var repetitionsCell = self.tableView.cellForRow(at: [0, 3]) as! JournalEntryTableViewDetailsCell
+    lazy var dateCell = self.tableView.cellForRow(at: [0, 4]) as! JournalEntryTableViewDateCell
     
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var testButton: UIButton!
     @IBOutlet weak var saveButton: UIButton!
+    @IBOutlet var gestureRecognizer: UITapGestureRecognizer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,30 +35,52 @@ class JournalEntryViewController: UIViewController, Storyboarded {
         self.tableView.dataSource = self
         
         self.saveButton.layer.cornerRadius = 25.0
+        
     }
         
     func unlockTextFields() {
-        for cell in [durationCell, distanceCell, repetitionsCell] {
-            cell.unlockTextField()
+        self.lockTextFields()
+        let cells = [durationCell, distanceCell, repetitionsCell]
+        let selectedActivityInstanceDetails = [self.selectedActivityInstance!.duration, self.selectedActivityInstance!.distance, self.selectedActivityInstance!.repetitions] as [Any?]
+        
+        for (index, cell) in cells.enumerated() {
+            if let _ = selectedActivityInstanceDetails[index] {
+                cell.unlockTextField()
+            }
+        }
+    }
+    
+    func lockTextFields() {
+        let cells = [durationCell, distanceCell, repetitionsCell]
+        cells.forEach({
+            $0.lockTextField()
+        })
+    }
+    
+    @objc func resignTextFields() {
+        for cell in [self.durationCell, self.distanceCell, self.repetitionsCell] {
+            if cell.textField.isFirstResponder {
+                cell.textField.resignFirstResponder()
+            }
         }
     }
 
+  
 }
 
 // MARK: - IBActions
 
 extension JournalEntryViewController {
     
+    @IBAction func gestureRecognizerAction(_ sender: Any) {
+        self.resignTextFields()
+    }
+    
     @IBAction func testButtonAction(_ sender: Any) {
         self.tableView.reloadData()
     }
     
     @IBAction func saveButtonAction(_ sender: Any) {
-//        let activityCell = self.tableView.cellForRow(at: [0, 0]) as! JournalEntryTableViewActivityCell
-//        let durationCell = self.tableView.cellForRow(at: [0, 1]) as! JournalEntryTableViewDetailsCell
-//        let distanceCell = self.tableView.cellForRow(at: [0, 2]) as! JournalEntryTableViewDetailsCell
-//        let repetitionsCell = self.tableView.cellForRow(at: [0, 3]) as! JournalEntryTableViewDetailsCell
-
         let reps = self.repetitionsCell.getNumber() == nil ? nil : Int(self.repetitionsCell.getNumber()!)
         let activity = self.activityCell.chosenActivity!.init(duration: self.durationCell.getNumber(), distance: self.distanceCell.getNumber(), repetitions: reps)
         
@@ -85,7 +105,8 @@ extension JournalEntryViewController: UITableViewDataSource {
             let cell = self.tableView.dequeueReusableCell(withIdentifier: JournalEntryTableViewActivityCell.id) as! JournalEntryTableViewActivityCell
             cell.coordinator = self.coordinator!
             cell.label.text = cell.cellLabel == nil ? "Activity" : cell.cellLabel!
-            cell.unlocker = { [weak self] in
+            cell.completion = { [weak self] activityType in
+                self?.selectedActivityInstance = activityType!.init()
                 self?.unlockTextFields()
             }
 
@@ -114,8 +135,13 @@ extension JournalEntryViewController: UITableViewDelegate {
         self.tableView.deselectRow(at: indexPath, animated: true)
         
         if indexPath.row == 0 {
-            let cell = self.tableView.cellForRow(at: indexPath) as! JournalEntryTableViewActivityCell
-            self.coordinator!.pushActivitiesListViewController(trigerredCell: cell)
+            self.coordinator!.pushActivitiesListViewController(trigerredCell: self.activityCell)
+        }
+        
+        if indexPath.row == 4 {
+//            let datePicker = UIDatePicker()
+//            self.dateCell.datePicker = datePicker
+            
         }
     }
 }
