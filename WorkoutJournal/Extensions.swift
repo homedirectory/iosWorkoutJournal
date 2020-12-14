8ce1edc7e4cf54f62557d9f6bb9769b93558cc68
vlad-bilyk <bilyk.v@ucu.edu.ua>
2020-12-14 15:30:11 +0200

    added Extensions file for various extensions

---
+++ WorkoutJournal/Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  Extensions.swift
+//  WorkoutJournal
+//
+//  Created by Vlad Bilyk on 14.12.2020.
+//  Copyright © 2020 Vlad Bilyk. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UITableViewCell {
+    static var id: String {
+        String(describing: Self.self)
+    }
+}
