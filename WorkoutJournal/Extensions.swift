046f8f27b8f5e93ea2d4bc6497c7fe75c2cf2735
vlad-bilyk <bilyk.v@ucu.edu.ua>
2020-12-16 17:34:25 +0200

    extended type Double by adding a few properties to get a String of a particular format

--- WorkoutJournal/Extensions.swift
+++ WorkoutJournal/Extensions.swift
@@ -20,3 +20,40 @@ extension UICollectionViewCell {
         String(describing: Self.self)
     }
 }
+
+extension Double {
+    var kmString: String {
+        var result = "-"
+        if Int((self / 100)) % 10 == 0 {
+            result = "\(Int(self/1000)) km"
+        }
+        else {
+            result = String(format: "%.1f km", self/1000)
+        }
+        return result
+    }
+    
+    var secondsString: String {
+        let hours = floor(self / 3600)
+        let minutes = (self - (hours * 3600)) / 60
+        var result = ""
+        if self >= 3600 {
+            result = "\(Int(hours)) h "
+        }
+        if self.truncatingRemainder(dividingBy: 3600) != 0 {
+            result += "\(Int(minutes)) min"
+        }
+        return result.isEmpty ? "-" : result
+    }
+    
+    var kmPerHourString: String {
+        var result = "-"
+        if (self * 10).truncatingRemainder(dividingBy: 10) == 0 {
+            result = "\(Int(self)) km/h"
+        }
+        else {
+            result = String(format: "%.1f km/h", self)
+        }
+        return result
+    }
+}
