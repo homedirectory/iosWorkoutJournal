c1796c4a33a4acbe3fd61abe7623d8e303d0cb00
vlad-bilyk <bilyk.v@ucu.edu.ua>
2020-12-16 15:30:02 +0200

    added AchievementViewController and AchievementCell

---
+++ WorkoutJournal/Controllers/ProfileTab/AchievementsViewController.swift
@@ -0,0 +1,43 @@
+//
+//  AchievementsViewController.swift
+//  WorkoutJournal
+//
+//  Created by Vlad Bilyk on 16.12.2020.
+//  Copyright © 2020 Vlad Bilyk. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class AchievementsViewController: UIViewController, Storyboarded {
+    
+    @IBOutlet weak var topLabel: UILabel!
+    @IBOutlet weak var tableLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+    }
+    
+}
+
+extension AchievementsViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
+}
+
+extension AchievementsViewController: UITableViewDelegate {
+    
+}
---
+++ WorkoutJournal/View/Cells/ProfileCollectionView/AchievementCell.swift
@@ -0,0 +1,15 @@
+//
+//  AchievementCell.swift
+//  WorkoutJournal
+//
+//  Created by Vlad Bilyk on 16.12.2020.
+//  Copyright © 2020 Vlad Bilyk. All rights reserved.
+//
+
+import UIKit
+
+class AchievementCell: UITableViewCell {
+
+    @IBOutlet weak var label: UILabel!
+    
+}
