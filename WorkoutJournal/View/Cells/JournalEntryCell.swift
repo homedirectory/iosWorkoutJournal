09ccb326cd5c50ca535f14e3fc0cc5edeb1a6217
vlad-bilyk <bilyk.v@ucu.edu.ua>
2020-12-16 20:16:00 +0200

    modified JournalEntryCell details label

--- WorkoutJournal/View/Cells/JournalEntryCell.swift
+++ WorkoutJournal/View/Cells/JournalEntryCell.swift
@@ -15,43 +15,37 @@ class JournalEntryCell: UITableViewCell {
     var entry: JournalEntry?
     
     #warning("TODO: fix image")
-    @IBOutlet weak var activityImage: UIImageView!
+    @IBOutlet weak var activityImageView: UIImageView!
     @IBOutlet weak var labelActivityName: UILabel!
-    @IBOutlet weak var labelDistance: UILabel!
-    @IBOutlet weak var labelDuration: UILabel!
-    @IBOutlet weak var labelRepetitions: UILabel!
-    
-    func configureDefaults() {
-        self.labelActivityName.text = ""
-        self.labelDistance.text = "Distance"
-        self.labelDuration.text = "Duration"
-        self.labelRepetitions.text = "Repetitions"
-        
-        self.labelDistance.isHidden = true
-        self.labelDuration.isHidden = true
-        self.labelRepetitions.isHidden = true
-    }
+    @IBOutlet weak var detailsLabel: UILabel!
     
     public func configureDetails() {
         guard let activity = self.entry?.activity else { return }
-        self.configureDefaults()
         
         self.labelActivityName.text = type(of: activity).name
         
+        var details = ""
+        
         if let distance = activity.distance {
-            self.labelDistance.text = distance.kmString
-            self.labelDistance.isHidden = false
+            details += distance.kmString
         }
         
         if let duration = activity.duration {
-            self.labelDuration.text = duration.secondsString
-            self.labelDuration.isHidden = false
+            if !details.isEmpty {
+                details += " | "
+            }
+            details += duration.secondsString
         }
         
         if let reps = activity.repetitions {
-            self.labelRepetitions.text = String(Int(reps))
-            self.labelRepetitions.isHidden = false
+            if !details.isEmpty {
+                details += " | "
+            }
+            details += String(Int(reps))
         }
+        
+        self.detailsLabel.text = details
+        self.detailsLabel.textColor = .gray
     }
     
     
