4559a356a6aa7f7773b02bd5b71bb1a70fe9606e
vlad-bilyk <bilyk.v@ucu.edu.ua>
2020-12-16 21:03:35 +0200

    added a static func to check if two dates are from the same day

--- WorkoutJournal/Model/StaticVariables.swift
+++ WorkoutJournal/Model/StaticVariables.swift
@@ -17,9 +17,26 @@ public class StaticVariables {
         $0.name
     })
     
-    
     static func getTotalTimeSpentOnActivities() -> Stats {
         return Stats(name: "Total time spent excercising", value: Running.totalDuration.value + Plank.totalDuration.value + ExcerciseSession.totalDuration.value, units: .seconds)
+    }
+    
+    static func datesFromSameDay(date1: Date, date2: Date) -> Bool {
+        let diff = Calendar.current.dateComponents([.day], from: date1, to: date2)
+        if diff.day == 0 {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    static let monthsNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    
+    static func dateComponentsToString(_ componenets: [Int]) -> String {
+        let day = componenets[0]
+        let monthString = Self.monthsNames[componenets[1] - 1]
+        let year = componenets[2]
+        return "\(day) \(monthString), \(year)"
     }
     
 }
