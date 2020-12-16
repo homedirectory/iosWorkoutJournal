4fc5aa22172a354302dc7bcb039f86a1f1091f0f
vlad-bilyk <bilyk.v@ucu.edu.ua>
2020-12-16 21:03:13 +0200

    added an extension to get Date components as integers

--- WorkoutJournal/Extensions.swift
+++ WorkoutJournal/Extensions.swift
@@ -57,3 +57,9 @@ extension Double {
         return result
     }
 }
+
+extension Date {
+    func get(_ component: Calendar.Component, calendar: Calendar = Calendar.current) -> Int {
+        return calendar.component(component, from: self)
+    }
+}
